875811b12fbe7534ff779e4be3a9277c00be9703
santiagoRicops <santuago231@gmail.com>
2023-10-18 18:10:25 -0500

    mi primer mensaje

---
+++ script.js
@@ -0,0 +1,74 @@
+// expresion
+// declaracion
+
+let seis = 3 + 3
+
+let J = "ss"
+
+let cinco = 3 + 2
+cinco + 1 // 6
+
+let n = "SANTIAGO"
+
+let ochoc = seis + 2
+
+var miVariable = "Hola"
+miVariable = 2
+
+let miVariablelet = "Hola"
+miVariablelet =""
+
+const miVariableConst = "Hola"
+window
+console.log(window)
+
+
+// primitivos
+// string
+// numeros
+// booleanos
+// null
+// undefined
+
+// compuesto
+// array
+// object
+ const seAcabo = null
+
+
+ console.log(seAcabo);
+const p = true
+
+2 / 2
+2 % 2
+
+// operqdores de comparacion
+console.log(2 == "2");
+console.log(2 === "2");
+console.log(2 != "2");
+console.log(2 !== "2");
+console.log(2 > 1);
+console.log(2 < 3 );
+console.log(2 <= 3);
+console.log(2 >= 3);
+
+// opreadores logicos
+// &&   AND
+// || OR
+// ! NOT
+
+console.log(2 > 4 && 4 > 2);
+
+console.log(9 >  8 && 8 > 7 && 2 > 1 && 1 > 2);
+console.log(9 > 10 || 10 > 9);
+console.log(!true);
+
+// null false
+// 0 false
+// ubdefined false
+// -1 false
+// faqlse false
+
+let modal = false
+
+console.log(!modal);
