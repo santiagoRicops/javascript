20d0890817f061bd7927aa30711fdc8b6d66c661
santiagoRicops <santuago231@gmail.com>
2023-10-20 17:49:14 -0500

    condicionales,funciones,promt

--- script.js
+++ script.js
@@ -65,6 +65,33 @@ console.log(9 >  8 && 8 > 7 && 2 > 1 && 1 > 2);
 console.log(9 > 10 || 10 > 9);
 console.log(!true);
 
+// decremento e incremento
+
+let j = 10
+
+++j
+if (7 !== "7" && 8 < 7 || 5 !== true){
+    console.log("es totalmente diferente");
+}
+if (true > false){
+    console.log("true es mayor");
+
+}
+if (true === false){
+    console.log("son iguales");
+}else{
+    console.log("son diferentes");
+}
+console.log(j++);
+
+if(true) {
+console.log('verdadero');
+} else {
+
+    console.log('falso');
+}
+
+
 // null false
 // 0 false
 // ubdefined false
@@ -74,3 +101,93 @@ console.log(!true);
 let modal = false
 
 console.log(!modal);
+
+const edad =  18;
+if (edad !== 18){
+    console.log("no tiene 18");
+}else{
+    console.log("tiene 18");
+}
+
+
+// funciones
+
+// QUE ES UNA FUNCION   
+
+function name_funcion() {
+console.log('mi primera funcion');
+}
+
+// ejecucion
+name_funcion()
+function suma(){
+    const numeroUno = 5;
+    const numerosDos = 10;
+     console.log(numeroUno+numerosDos); 
+
+}
+suma();
+suma()
+suma()
+suma()
+suma()
+
+function params(numeroUno, numerosDos) {
+  const resultado =  numeroUno + numerosDos
+  console.log(resultado)
+
+}
+
+params(2, 5) 
+params(10, 5) 
+params(10, 5) 
+params(10, 5) 
+params(10, 5) 
+params(10, 5) 
+params(10, 5) 
+function nombreUsuario(nombre, apellido) {
+    const nombreCompleto = nombre + " " + apellido
+    return nombreCompleto
+    4
+    SVGDefsElementasd
+    asdasd
+    apellido
+}
+
+console.log(nombreUsuario("sebastian", "garcia") + 10);
+nombreUsuario("santiago", "rico")
+
+
+let minumero = 10
+
+function minumerofuncion() {
+    return 5
+}
+console.log(minumerofuncion() + minumero); 
+
+function paramsWithReturn(param) {
+ 
+    return param
+}
+
+paramsWithReturn(5)
+
+//console.log(prompt("Mi argumento para prompt"))
+ const input =  prompt("ingrese el nombre de usuario")
+function nombreCliente(cliente){
+   
+    if("") {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+   if (cliente){
+    console.log(cliente) 
+   }else{
+    console.log("vuelva pronto")
+   }
+}
+nombreCliente(input)
+
+console.log("" === false);
+// hacer una funcion que se llame nombre de cliente y reciba un parametro llamado cliente debe de venir de un prom si le doy aceptar me da el nombre del usuario y si le doy cancelar me dice vuelva pronto
